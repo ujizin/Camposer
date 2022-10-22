9ac2fe7a7a60c9ae835de4e94c5965407815560e
devLucasYuji <lucasyujideveloper@gmail.com>
2022-10-22 01:47:59 -0300

    feat: remove indication from option section

--- sample/src/main/java/br/com/devlucasyuji/sample/ui/preview/components/OptionSection.kt
+++ sample/src/main/java/br/com/devlucasyuji/sample/ui/preview/components/OptionSection.kt
@@ -1,10 +1,12 @@
 package br.com.devlucasyuji.sample.ui.preview.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +30,10 @@ fun OptionSection(
         Option.values().forEach { option ->
             val capMode = option.toCaptureMode()
             Text(
-                modifier = Modifier.clickable { onOptionChanged(capMode) },
+                modifier = Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) { onOptionChanged(capMode) },
                 text = stringResource(id = option.titleRes).replaceFirstChar { it.uppercase() },
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
