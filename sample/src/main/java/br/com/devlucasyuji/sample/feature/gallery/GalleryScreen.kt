89908aa1c1b4efbfeaf46b95488cbb35ff683da0
devLucasYuji <lucasyujideveloper@gmail.com>
2022-10-25 20:32:41 -0300

    feat: add gallery loading

--- sample/src/main/java/br/com/devlucasyuji/sample/feature/gallery/GalleryScreen.kt
+++ sample/src/main/java/br/com/devlucasyuji/sample/feature/gallery/GalleryScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,44 +23,55 @@ import coil.compose.AsyncImage
 import coil.decode.VideoFrameDecoder
 import coil.request.ImageRequest
 import coil.request.videoFrameMillis
+import java.io.File
 
-
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GalleryScreen(viewModel: GalleryViewModel = viewModel()) {
     val uiState by viewModel.uiState.collectAsState()
 
     when (val result: GalleryUiState = uiState) {
-        GalleryUiState.Initial -> Box {}
-        is GalleryUiState.Success ->
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(3),
-                Modifier.fillMaxSize()
-            ) {
-                items(result.images, { it.name }) { image ->
-                    Box(
-                        modifier = Modifier
-                            .padding(1.dp)
-                            .fillMaxSize()
+        GalleryUiState.Initial -> GalleryLoading()
+        is GalleryUiState.Success -> GallerySection(imageFiles = result.images)
 
-                            .animateItemPlacement(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1F),
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(image)
-                                .decoderFactory(VideoFrameDecoder.Factory())
-                                .videoFrameMillis(1)
-                                .build(),
-                            contentScale = ContentScale.Crop,
-                            contentDescription = "images",
-                            alignment = Alignment.Center,
-                        )
-                    }
-                }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun GallerySection(imageFiles: List<File>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        Modifier.fillMaxSize()
+    ) {
+        items(imageFiles, { it.name }) { image ->
+            Box(
+                modifier = Modifier
+                    .padding(1.dp)
+                    .fillMaxSize()
+                    .animateItemPlacement(),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1F),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(image)
+                        .decoderFactory(VideoFrameDecoder.Factory())
+                        .videoFrameMillis(1)
+                        .build(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "images",
+                    alignment = Alignment.Center,
+                )
             }
+        }
+    }
+}
+
+@Composable
+private fun GalleryLoading() {
+    Box(Modifier.fillMaxSize()) {
+        CircularProgressIndicator()
     }
 }
