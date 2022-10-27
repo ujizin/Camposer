89b08916ecc4b47fb97e531b6ed74e884243fda4
DevLucasYuji <lucasyujideveloper@gmail.com>
2022-10-27 15:07:44 -0300

    fix: circle ripple on picture button

--- sample/src/main/java/br/com/devlucasyuji/sample/feature/camera/components/PictureActions.kt
+++ sample/src/main/java/br/com/devlucasyuji/sample/feature/camera/components/PictureActions.kt
@@ -3,22 +3,13 @@ package br.com.devlucasyuji.sample.feature.camera.components
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -126,9 +117,9 @@ private fun PictureButton(
         modifier = Modifier
             .size(80.dp)
             .border(BorderStroke(4.dp, Color.White), CircleShape)
-            .clip(CircleShape)
-            .padding(12.dp)
+            .padding(8.dp)
             .background(color, CircleShape)
+            .clip(CircleShape)
             .then(modifier),
         onClick = onClick
     )
