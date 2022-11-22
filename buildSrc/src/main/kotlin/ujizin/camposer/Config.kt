92a146fde3abf6def20ca2060c1eb4d45b3a8282
ujizin <lucasyujideveloper@gmail.com>
2022-11-21 21:38:29 -0300

    build: change min sdk to 23

--- buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
+++ buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
@@ -3,7 +3,7 @@ package com.ujizin.camposer
 object Config {
     const val compileSdk = 33
     const val targetSdk = 33
-    const val minSdk = 24
+    const val minSdk = 23
     const val versionCode = 1
     const val versionName = "0.1.0"
     const val groupId = "com.github.ujizin"
