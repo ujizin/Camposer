fda9fcd549075c1b0f6a980bf12e2df47444a2ca
Lucas Yuji Yoshimine <51065868+ujizin@users.noreply.github.com>
2023-06-09 17:15:42 -0300

    build: bump release 0.2.2

--- buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
+++ buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
@@ -4,8 +4,8 @@ object Config {
     const val compileSdk = 33
     const val targetSdk = 33
     const val minSdk = 21
-    const val versionCode = 4
-    const val versionName = "0.2.1"
+    const val versionCode = 5
+    const val versionName = "0.2.2"
     const val groupId = "io.github.ujizin"
     const val artifactId = "camposer"
 }
