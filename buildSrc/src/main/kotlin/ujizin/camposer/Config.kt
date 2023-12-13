b96934c7d00b4d9560abe0ca851fd59ff510876e
ujizin <lucasyujideveloper@gmail.com>
2023-12-13 18:16:49 +0000

    build: bump version to 0.3.1 (7)

--- buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
+++ buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
@@ -4,8 +4,8 @@ object Config {
     const val compileSdk = 33
     const val targetSdk = 33
     const val minSdk = 21
-    const val versionCode = 6
-    const val versionName = "0.3.0"
+    const val versionCode = 7
+    const val versionName = "0.3.1"
     const val groupId = "io.github.ujizin"
     const val artifactId = "camposer"
 }
