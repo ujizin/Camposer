07e7322b37aaeacf7d525de0845059aa236590e0
Vansh Gandhi <vansh@smileidentity.com>
2023-12-22 11:30:31 -0800

    Update target and compile sdk

--- buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
+++ buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
@@ -1,8 +1,8 @@
 package com.ujizin.camposer
 
 object Config {
-    const val compileSdk = 33
-    const val targetSdk = 33
+    const val compileSdk = 34
+    const val targetSdk = 34
     const val minSdk = 21
     const val versionCode = 8
     const val versionName = "0.3.2"
