0e7b2a7898a757a0fcf1919490346641471df000
ujizin <lucasyujideveloper@gmail.com>
2025-08-30 00:46:46 +0100

    build: add sample ios app

---
+++ sampleIosApp/sampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleIosApp
+//
+//  Created by Lucas Yuji Yoshimine on 29/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleIosApp/sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleIosApp
+//
+//  Created by Lucas Yuji Yoshimine on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
