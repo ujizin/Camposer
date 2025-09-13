31de6b04024587a1cbb51e55f82df4769550f732
ujizin <lucasyujideveloper@gmail.com>
2025-09-13 17:47:21 +0100

    build: bump version to 0.4.5 (14)

--- buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
+++ buildSrc/src/main/kotlin/ujizin/camposer/Config.kt
@@ -4,8 +4,8 @@ object Config {
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 21
-    const val versionCode = 13
-    const val versionName = "0.4.4"
+    const val versionCode = 14
+    const val versionName = "0.4.5"
     const val groupId = "io.github.ujizin"
     const val artifactId = "camposer"
 }
