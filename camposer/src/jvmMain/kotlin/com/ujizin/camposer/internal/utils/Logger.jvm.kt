6a292713c6989b3f85bedcea461756f4dab08243
ujizin <lucasyujideveloper@gmail.com>
2026-03-01 18:13:20 +0000

    fix(jvm): align Logger.jvm.kt style with android/ios actuals (KDoc, TAG constant, 2-space indent)

--- camposer/src/jvmMain/kotlin/com/ujizin/camposer/internal/utils/Logger.jvm.kt
+++ camposer/src/jvmMain/kotlin/com/ujizin/camposer/internal/utils/Logger.jvm.kt
@@ -1,12 +1,22 @@
 package com.ujizin.camposer.internal.utils
 
+/**
+ * JVM implementation of Camposer logger.
+ * Writes debug output to stdout and errors to stderr.
+ * Note: debug logging is unconditional on JVM desktop (no release-build stripping equivalent).
+ */
 internal actual object Logger {
-    actual fun d(message: String) {
-        println("[Camposer] DEBUG: $message")
-    }
+  private const val TAG = "Camposer"
 
-    actual fun error(message: String, throwable: Throwable?) {
-        System.err.println("[Camposer] ERROR: $message")
-        throwable?.printStackTrace(System.err)
-    }
+  actual fun d(message: String) {
+    println("[$TAG] DEBUG: $message")
+  }
+
+  actual fun error(
+    message: String,
+    throwable: Throwable?,
+  ) {
+    System.err.println("[$TAG] ERROR: $message")
+    throwable?.printStackTrace(System.err)
+  }
 }
