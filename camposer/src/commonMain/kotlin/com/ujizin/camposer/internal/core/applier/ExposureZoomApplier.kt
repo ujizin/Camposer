18b350c12003fe9c987c08b5245441e27b13d0f5
ujizin <lucasyujideveloper@gmail.com>
2026-05-10 22:09:04 +0100

    refactor: introduce expect/actual for ExposureZoomApplier

---
+++ camposer/src/commonMain/kotlin/com/ujizin/camposer/internal/core/applier/ExposureZoomApplier.kt
@@ -0,0 +1,13 @@
+package com.ujizin.camposer.internal.core.applier
+
+import com.ujizin.camposer.state.properties.FlashMode
+
+internal expect class ExposureZoomApplier : CameraStateApplier {
+  fun applyFlashMode(flashMode: FlashMode)
+
+  fun applyExposureCompensation(exposureCompensation: Float)
+
+  fun applyZoomRatio(zoomRatio: Float)
+
+  fun applyTorchEnabled(isTorchEnabled: Boolean)
+}
