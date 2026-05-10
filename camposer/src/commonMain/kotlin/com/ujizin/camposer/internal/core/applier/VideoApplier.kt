a530f9d563a9eeb7c1981fb457140d0998bf89e4
ujizin <lucasyujideveloper@gmail.com>
2026-05-10 22:05:51 +0100

    refactor: introduce expect/actual for VideoApplier

---
+++ camposer/src/commonMain/kotlin/com/ujizin/camposer/internal/core/applier/VideoApplier.kt
@@ -0,0 +1,12 @@
+package com.ujizin.camposer.internal.core.applier
+
+import com.ujizin.camposer.state.properties.ImageCaptureStrategy
+import com.ujizin.camposer.state.properties.VideoStabilizationMode
+
+internal expect class VideoApplier : CameraStateApplier {
+  fun applyImageCaptureStrategy(imageCaptureStrategy: ImageCaptureStrategy)
+
+  fun applyFrameRate(frameRate: Int)
+
+  fun applyVideoStabilizationMode(videoStabilizationMode: VideoStabilizationMode)
+}
