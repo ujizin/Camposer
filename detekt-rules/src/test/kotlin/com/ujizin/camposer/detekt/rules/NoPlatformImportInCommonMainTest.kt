842917c04a7852ac5046afb694d78a28efb30590
ujizin <lucasyujideveloper@gmail.com>
2026-05-17 12:39:09 +0100

    feat(detekt): add IdempotencyGuardRequired rule

--- detekt-rules/src/test/kotlin/com/ujizin/camposer/detekt/rules/NoPlatformImportInCommonMainTest.kt
+++ detekt-rules/src/test/kotlin/com/ujizin/camposer/detekt/rules/NoPlatformImportInCommonMainTest.kt
@@ -5,66 +5,65 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class NoPlatformImportInCommonMainTest {
+  private val rule = NoPlatformImportInCommonMain()
 
-    private val rule = NoPlatformImportInCommonMain()
-
-    @Test
-    fun `flags androidx camera import`() {
-        val code = """
+  @Test
+  fun `flags androidx camera import`() {
+    val code = """
             package com.ujizin.camposer
             import androidx.camera.core.ImageCapture
             class Foo
-        """.trimIndent()
-        assertEquals(1, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(1, rule.lint(code).size)
+  }
 
-    @Test
-    fun `flags platform AVFoundation import`() {
-        val code = """
+  @Test
+  fun `flags platform AVFoundation import`() {
+    val code = """
             package com.ujizin.camposer
             import platform.AVFoundation.AVCaptureSession
             class Foo
-        """.trimIndent()
-        assertEquals(1, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(1, rule.lint(code).size)
+  }
 
-    @Test
-    fun `flags UIKit import`() {
-        val code = """
+  @Test
+  fun `flags UIKit import`() {
+    val code = """
             package com.ujizin.camposer
             import UIKit.UIView
             class Foo
-        """.trimIndent()
-        assertEquals(1, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(1, rule.lint(code).size)
+  }
 
-    @Test
-    fun `does not flag compose import`() {
-        val code = """
+  @Test
+  fun `does not flag compose import`() {
+    val code = """
             package com.ujizin.camposer
             import androidx.compose.runtime.Composable
             class Foo
-        """.trimIndent()
-        assertEquals(0, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(0, rule.lint(code).size)
+  }
 
-    @Test
-    fun `does not flag camposer internal import`() {
-        val code = """
+  @Test
+  fun `does not flag camposer internal import`() {
+    val code = """
             package com.ujizin.camposer
             import com.ujizin.camposer.state.CameraState
             class Foo
-        """.trimIndent()
-        assertEquals(0, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(0, rule.lint(code).size)
+  }
 
-    @Test
-    fun `does not flag kotlin stdlib import`() {
-        val code = """
+  @Test
+  fun `does not flag kotlin stdlib import`() {
+    val code = """
             package com.ujizin.camposer
             import kotlin.math.roundToInt
             class Foo
-        """.trimIndent()
-        assertEquals(0, rule.lint(code).size)
-    }
+    """.trimIndent()
+    assertEquals(0, rule.lint(code).size)
+  }
 }
