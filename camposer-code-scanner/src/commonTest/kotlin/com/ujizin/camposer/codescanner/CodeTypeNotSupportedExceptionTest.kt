43b87bf65366a9b74a9fbdea31fc40e27fa975c0
ujizin <lucasyujideveloper@gmail.com>
2026-05-20 23:24:55 +0100

    test(code-scanner): add CodeResult and CodeTypeNotSupportedException unit tests

---
+++ camposer-code-scanner/src/commonTest/kotlin/com/ujizin/camposer/codescanner/CodeTypeNotSupportedExceptionTest.kt
@@ -0,0 +1,40 @@
+package com.ujizin.camposer.codescanner
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+internal class CodeTypeNotSupportedExceptionTest {
+  @Test
+  fun codeType_property_preserved() {
+    val ex = CodeTypeNotSupportedException(CodeType.Aztec)
+    assertEquals(CodeType.Aztec, ex.codeType)
+  }
+
+  @Test
+  fun default_message_contains_code_type_name() {
+    val ex = CodeTypeNotSupportedException(CodeType.QRCode)
+    assertNotNull(ex.message)
+    assertTrue(ex.message!!.contains("QRCode"))
+  }
+
+  @Test
+  fun custom_message_overrides_default() {
+    val ex = CodeTypeNotSupportedException(CodeType.QRCode, "custom error")
+    assertEquals("custom error", ex.message)
+  }
+
+  @Test
+  fun is_exception_subtype() {
+    assertTrue(CodeTypeNotSupportedException(CodeType.QRCode) is Exception)
+  }
+
+  @Test
+  fun all_code_types_produce_non_null_message() {
+    CodeType.entries.forEach { type ->
+      val ex = CodeTypeNotSupportedException(type)
+      assertNotNull(ex.message)
+    }
+  }
+}
