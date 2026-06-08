eae4be24b47d6ee8d68eca4603c970fcbbb9d1ee
ujizin <lucasyujideveloper@gmail.com>
2026-06-08 21:59:37 +0100

    fix(camera-session): walk up context wrapper until finds lifecycle owner in order to avoid crash

--- camposer/src/androidMain/kotlin/com/ujizin/camposer/internal/extensions/ContextExtensions.kt
+++ camposer/src/androidMain/kotlin/com/ujizin/camposer/internal/extensions/ContextExtensions.kt
@@ -1,9 +1,11 @@
 package com.ujizin.camposer.extensions
 
 import android.content.Context
+import android.content.ContextWrapper
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.LifecycleOwner
 import java.util.concurrent.Executor
 
 private class MainThreadExecutor(
@@ -20,6 +22,13 @@ private class MainThreadExecutor(
     handler.post(r)
   }
 }
+
+internal tailrec fun Context.findLifecycleOwner(): LifecycleOwner =
+  when (this) {
+    is LifecycleOwner -> this
+    is ContextWrapper -> baseContext.findLifecycleOwner()
+    else -> error("No LifecycleOwner found in Context chain")
+  }
 
 internal val Context.compatMainExecutor: Executor
   get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
