d1563c86d21a9bb18c994bc29dd97ce7e84a925e
ujizin <lucasyujideveloper@gmail.com>
2026-06-09 01:00:12 +0100

    refactor(camera-session): remove unused findLifecycleOwner extension

--- camposer/src/androidMain/kotlin/com/ujizin/camposer/internal/extensions/ContextExtensions.kt
+++ camposer/src/androidMain/kotlin/com/ujizin/camposer/internal/extensions/ContextExtensions.kt
@@ -1,11 +1,9 @@
 package com.ujizin.camposer.extensions
 
 import android.content.Context
-import android.content.ContextWrapper
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import androidx.lifecycle.LifecycleOwner
 import java.util.concurrent.Executor
 
 private class MainThreadExecutor(
@@ -22,13 +20,6 @@ private class MainThreadExecutor(
     handler.post(r)
   }
 }
-
-internal tailrec fun Context.findLifecycleOwner(): LifecycleOwner =
-  when (this) {
-    is LifecycleOwner -> this
-    is ContextWrapper -> baseContext.findLifecycleOwner()
-    else -> error("No LifecycleOwner found in Context chain")
-  }
 
 internal val Context.compatMainExecutor: Executor
   get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
