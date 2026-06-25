03d59bb2c9666faeb58ae3331333bde2672635b5
ujizin <lucasyujideveloper@gmail.com>
2026-06-25 22:59:05 +0100

    fix(gesture): avoid perform gestures action when camera is not streaming

--- camposer/src/iosMain/kotlin/com/ujizin/camposer/internal/view/gesture/PinchToZoomGestureHandler.kt
+++ camposer/src/iosMain/kotlin/com/ujizin/camposer/internal/view/gesture/PinchToZoomGestureHandler.kt
@@ -22,6 +22,7 @@ internal class PinchToZoomGestureHandler(
   @OptIn(BetaInteropApi::class)
   @ObjCAction
   internal fun onPinch(sender: UIPinchGestureRecognizer) {
+    if (!cameraSession.isStreaming) return
     if (!cameraSession.state.isPinchToZoomEnabled.value ||
       sender.state != UIGestureRecognizerStateChanged
     ) {
