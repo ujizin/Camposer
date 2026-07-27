916496e690f76a2dedff1ebf5bc4f3abd01d9159
ujizin <lucasyujideveloper@gmail.com>
2026-07-27 22:56:30 +0100

    fix(sample): route iOS entry point through SampleNavigation
    iOS was going directly to CameraScreen, skipping the permissions
    screen that Android shows via SampleNavigation.

--- samples/sample-multiplatform/shared/src/iosMain/kotlin/com/ujizin/camposer/shared/CameraViewController.ios.kt
+++ samples/sample-multiplatform/shared/src/iosMain/kotlin/com/ujizin/camposer/shared/CameraViewController.ios.kt
@@ -1,14 +1,11 @@
 package com.ujizin.camposer.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.ujizin.camposer.shared.features.camera.CameraScreen
-import com.ujizin.camposer.shared.theme.CamposerTheme
+import com.ujizin.camposer.shared.navigation.SampleNavigation
 import platform.UIKit.UIViewController
 
 @Suppress("FunctionName")
 fun CameraViewController(): UIViewController =
   ComposeUIViewController {
-    CamposerTheme {
-      CameraScreen()
-    }
+    SampleNavigation()
   }
